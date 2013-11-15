08f569b8d7ac8ac276acd2035f3ec8c1935ba6d5
themarkappleby <themarkappleby@gmail.com>
2013-11-15 16:43:12 -0500

    converted from Middleman to Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Styleguide::Application.config.secret_key_base = '506381debb787ac9f662028bd06615648d6a8df9143dfd495bd19468ddf0bbd32ef1171c285824bd9d432790a822bed26c1f9535ea4a471e0606392e5d9cba4b'
