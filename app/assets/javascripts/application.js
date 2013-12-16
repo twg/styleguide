6c88cd78926197a43ec5570f7fb6a32cc18ba456
themarkappleby <themarkappleby@gmail.com>
2013-12-16 14:39:06 -0500

    stripped RSportz specific formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,12 @@
 //= require "styleguide/_navigation"
 //= require_tree "./objects/"
 //= require_tree "./plugins/"
-//= require "_initializers.js"
-//= require_tree "./registrations/"
-//= require jquery_ujs
+
+// initialize js plugins
+$(document).ready(function() {
+    // init fastclick for removing iOS touch delay
+    FastClick.attach(document.body);
+
+    // init placeholder text polyfill for IE
+    $('input, textarea').placeholder();
+});
--- app/assets/javascripts/registrations/login_register.js.coffee
+++
@@ -1,5 +0,0 @@
-jQuery ->
-  return unless $('.org_registrations_accounts-page.login_register-action').length
-  
-  # :input selector selects input/textarea/select/button elements (http://api.jquery.com/input-selector/)
-  $('#form-preview').find(':input').prop('disabled', true)
