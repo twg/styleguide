15fb00049d1bdb91cced9f3c7707b8bf5612e822
themarkappleby <themarkappleby@gmail.com>
2016-01-18 17:50:33 -0500

    total revamp

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-
-  def icon(type, text = '', options = {})
-    options[:class] ||= ''
-    options[:class] << " icon icon-#{type}"
-    (content_tag(:i, '', options) + ' ' + text).html_safe
-  end
-
-end
